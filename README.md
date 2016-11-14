49d8d3b404b808e0adc2cd95d190c0728ee59db8
youkaisteve <youkaisteve@gmail.com>
2016-11-14 18:13:06 +0800

    add

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-native-picker
 cross platform picker for IOS and Android
+
+updating...
---
+++ lang/index.js
@@ -0,0 +1,8 @@
+import zhCN from './zh-CN.json';
+import enUS from './en-US.json';
+
+let langs = [];
+langs['zh-CN'] = zhCN;
+langs['en-US'] = enUS;
+
+export default langs;
