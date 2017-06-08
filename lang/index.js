98224f402cc9a286fdb6b4bf477a46e33e8cd792
Francisco Calderón <calderon@protonmail.com>
2017-06-08 16:31:52 -0300

    New spanish language

--- lang/index.js
+++ lang/index.js
@@ -1,8 +1,10 @@
 import zhCN from './zh-CN.json';
 import enUS from './en-US.json';
+import esAR from './es-AR.json';
 
 let langs = [];
 langs['zh-CN'] = zhCN;
 langs['en-US'] = enUS;
+langs['es-AR'] = esAR;
 
 export default langs;
